2700e0e76e28a2312822e5ce4b6e4083ddb6b9ef
LARSJava <l2002477157as@gmail.com>
2024-10-06 16:40:06 +0900

    Merge remote-tracking branch 'origin/kadai5' into kadai5
    # Conflicts:
    #	src/main/java/raisetech/StudentManagement/StudentManagementApplication.java

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourses;
@@ -38,4 +40,9 @@ public class StudentController {
   public List<StudentCourses> getStudentCourseList(){
       return service.searchStudentCourseList();
     };
+
+  @PostMapping("/registerStudent")
+  public String registerStudent(@ModelAttribute StudentDetail studentDetail){
+    return a;
+  }
 }
