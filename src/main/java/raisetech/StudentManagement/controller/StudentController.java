fae78a54466d931874308e80ae474af01f30be58
LARSJava <l2002477157as@gmail.com>
2024-10-06 16:02:15 +0900

    Merge remote-tracking branch 'origin/kadai5' into kadai5
    # Conflicts:
    #	src/main/java/raisetech/StudentManagement/StudentManagementApplication.java

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,19 +1,18 @@
 package raisetech.StudentManagement.controller;
 
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourses;
 import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
-@RestController
+@Controller
 
 public class StudentController {
 
@@ -27,11 +26,12 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList(){
+  public String getStudentList(Model model){
     List<Student> students = service.searchStudentList();
     List<StudentCourses> studentCourses = service.searchStudentCourseList();
 
-    return converter.convertStudentDetails(students, studentCourses);
+    model.addAttribute("studentList",converter.convertStudentDetails(students,studentCourses));
+    return "studentList";
   }
 
   @GetMapping("/studentcourses")
