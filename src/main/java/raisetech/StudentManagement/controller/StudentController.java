8a2659a3de74b7de47f8d884b0e3b213e89527e6
LARSJava <l2002477157as@gmail.com>
2024-11-17 15:38:22 +0900

    Merge remote-tracking branch 'origin/kadai5' into kadai5
    # Conflicts:
    #	src/main/java/raisetech/StudentManagement/StudentManagementApplication.java

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -1,8 +1,8 @@
 package raisetech.StudentManagement.controller;
 
-
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,7 +50,7 @@ public class StudentController {
     @return 受講生
    */
   @GetMapping("/student/{id}")
-  public StudentDetail getStudent(@PathVariable @Size(min = 1, max = 3) String id){
+  public StudentDetail getStudent(@PathVariable @NotBlank @Pattern(regexp = "\\d+$") String id){
     return service.searchStudent(id);
   }
 
@@ -61,7 +61,7 @@ public class StudentController {
    @return 実行結果
    */
   @PostMapping("/registerStudent")
-  public ResponseEntity<StudentDetail> registerStudent(@RequestBody StudentDetail studentDetail){
+  public ResponseEntity<StudentDetail> registerStudent(@RequestBody @Valid StudentDetail studentDetail){
     StudentDetail responseStudentDetail = service.registerStudent(studentDetail);
      return ResponseEntity.ok(responseStudentDetail);
   }
@@ -73,7 +73,7 @@ public class StudentController {
   @return 実行結果
  */
   @PutMapping("/updateStudent")
-  public ResponseEntity<String> updateStudent(@RequestBody StudentDetail studentDetail) {
+  public ResponseEntity<String> updateStudent(@RequestBody @Valid StudentDetail studentDetail) {
     service.updateStudent(studentDetail);
     return ResponseEntity.ok("更新処理が成功しました。");
   }
